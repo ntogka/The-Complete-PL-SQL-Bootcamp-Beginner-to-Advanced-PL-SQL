1db55b3c1ba7f62c5c33b6cdd59f19fa99b2c4c1
Marina Ntogka <marinadoga98@gmail.com>
2021-12-29 12:35:20 +0200

    Add files via upload

---
+++ 11. PLSQL Debugging Debugging Your Codes & Subprograms/grant-debug-privilege.sql
@@ -0,0 +1,15 @@
+/* 
+1)You need to connect with the "SYS" or "SYSTEM" user, 
+2)Execute the following codes to be able to debug your PL/SQL codes & subprograms.
+*/
+
+GRANT DEBUG CONNECT SESSION TO hr;
+GRANT DEBUG ANY PROCEDURE TO hr;
+
+BEGIN
+    dbms_network_acl_admin.append_host_ace
+           (host=>'127.0.0.1',
+            ace=> sys.xs$ace_type(privilege_list=>sys.xs$name_list('JDWP') ,
+                           principal_name=>'HR',
+                           principal_type=>sys.xs_acl.ptype_db) );
+END;
